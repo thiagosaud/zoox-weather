faee333f04b1daaa2e0c275cb1c70e326bc16f0c
THIAGO DE BONIS <thiagobonis@outlook.com>
2020-07-14 18:42:46 -0300

    Start Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'zx-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'zoox-weather';
+	title = 'zoox-weather';
 }
