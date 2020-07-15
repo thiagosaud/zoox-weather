83060c122550d66a1fa75a2dde7bd862e46d6bdf
THIAGO DE BONIS <thiagobonis@outlook.com>
2020-07-15 19:06:44 -0300

    Updated Search Page Component

--- src/app/pages/search-page/search-page.component.ts
+++ src/app/pages/search-page/search-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { debounceTime, map } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
 	selector: 'zx-search-page',
@@ -6,7 +9,108 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./search-page.component.scss'],
 })
 export class SearchPageComponent implements OnInit {
-	constructor() {}
+	searchForm: FormGroup;
+	listTest$ = new BehaviorSubject<Array<{ text: string; isCountry: boolean; isClicked: boolean }> | null>([
+		{
+			text: 'Rio de Janeiro',
+			isCountry: false,
+			isClicked: false,
+		},
+		{
+			text: 'Estados Unidos',
+			isCountry: true,
+			isClicked: false,
+		},
+		{
+			text: 'Brasil',
+			isCountry: true,
+			isClicked: false,
+		},
+		{
+			text: 'São Paulo',
+			isCountry: false,
+			isClicked: false,
+		},
+		{
+			text: 'Nova York',
+			isCountry: false,
+			isClicked: false,
+		},
+	]);
+	itemSelected: Array<{ text: string; isCountry: boolean; isClicked: boolean }> = [];
+
+	constructor(protected readonly fb: FormBuilder) {
+		this.searchForm = fb.group({
+			search: fb.control('', [Validators.minLength(1), Validators.maxLength(58), Validators.required]),
+		});
+	}
 
 	ngOnInit(): void {}
+
+	/** @description Returns the number of items selected from the list. */
+	get amountItemSelected(): number {
+		return this.listTest$.getValue().filter(item => item.isClicked).length;
+	}
+
+	/** @description Sort the list by pressing the button of the child component, the sorting can be of two types: "Ascending" or "Descending". */
+	orderByList(isOrderByAsc: boolean): void {
+		this.listTest$.getValue().sort((a, b) => (isOrderByAsc ? (a.text > b.text ? 1 : -1) : b.text > a.text ? 1 : -1));
+	}
+
+	/** @description Filters the list according to the characters entered in the child component. */
+	protected filterData(): void {
+		this.searchForm.valueChanges
+			.pipe(
+				debounceTime(500),
+				map(({ search }) => {
+					const searchNormalized = this.normalizeFilter(search);
+					const filterNormalized = this.listTest$.getValue().filter(item => this.normalizeFilter(item.text).includes(searchNormalized));
+
+					if (filterNormalized.length === 0 || !search) {
+						this.listTest$.next([
+							{
+								text: 'Rio de Janeiro',
+								isCountry: false,
+								isClicked: false,
+							},
+							{
+								text: 'Estados Unidos',
+								isCountry: true,
+								isClicked: false,
+							},
+							{
+								text: 'Brasil',
+								isCountry: true,
+								isClicked: false,
+							},
+							{
+								text: 'São Paulo',
+								isCountry: false,
+								isClicked: false,
+							},
+							{
+								text: 'Nova York',
+								isCountry: false,
+								isClicked: false,
+							},
+						]);
+
+						return this.listTest$.getValue();
+					}
+
+					return filterNormalized;
+				})
+			)
+			.subscribe(result => this.listTest$.next(result));
+	}
+
+	/** @description Normalizes the filter string, removing blank and seated characters. */
+	protected normalizeFilter(text: string): string {
+		return text
+			.toLowerCase()
+			.normalize('NFD')
+			.replace(/[\u0300-\u036f/\s]/g, '');
+	}
+
+	goToViewRoute() {}
 }
