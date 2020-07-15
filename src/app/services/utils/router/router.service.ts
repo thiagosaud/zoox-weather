c00963f8357880cae12abf999cb96fd4af5f98ec
THIAGO DE BONIS <thiagobonis@outlook.com>
2020-07-15 19:07:03 -0300

    Updated Route Service

--- src/app/services/utils/router/router.service.ts
+++ src/app/services/utils/router/router.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
+import { Router, NavigationEnd, NavigationExtras } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
@@ -7,6 +7,8 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class RouterUtilsService {
 	routerState$ = new BehaviorSubject({
+		error: false,
+		login: false,
 		home: false,
 		profile: false,
 		create: false,
@@ -27,32 +29,40 @@ export class RouterUtilsService {
 	/** @description Updates the state of the routes. */
 	protected setRouteStateConfig(routerName: string): void {
 		switch (routerName) {
+			case '/error':
+				this.updateRouterState(true, false, false, false, false, false, false, false, false);
+				break;
+			case '/login':
+				this.updateRouterState(false, true, false, false, false, false, false, false, false);
+				break;
 			case '/profile':
-				this.updateRouterState(false, true, false, false, false, false, false);
+				this.updateRouterState(false, false, false, true, false, false, false, false, false);
 				break;
 			case '/create':
-				this.updateRouterState(false, false, true, false, false, false, false);
+				this.updateRouterState(false, false, false, false, true, false, false, false, false);
 				break;
 			case '/create/country':
-				this.updateRouterState(false, false, false, true, false, false, false);
+				this.updateRouterState(false, false, false, false, false, true, false, false, false);
 				break;
 			case '/create/city':
-				this.updateRouterState(false, false, false, false, true, false, false);
+				this.updateRouterState(false, false, false, false, false, false, true, false, false);
 				break;
 			case '/search':
-				this.updateRouterState(false, false, false, false, false, true, false);
+				this.updateRouterState(false, false, false, false, false, false, false, true, false);
 				break;
 			case '/view':
-				this.updateRouterState(false, false, false, false, false, false, true);
+				this.updateRouterState(false, false, false, false, false, false, false, false, true);
 				break;
 			default:
-				this.updateRouterState(true, false, false, false, false, false, false);
+				this.updateRouterState(false, false, true, false, false, false, false, false, false);
 				break;
 		}
 	}
 
 	/** @description Auxiliary method for updating route states */
 	protected updateRouterState(
+		error: boolean,
+		login: boolean,
 		home: boolean,
 		profile: boolean,
 		create: boolean,
@@ -62,6 +72,8 @@ export class RouterUtilsService {
 		view: boolean
 	): void {
 		this.routerState$.next({
+			error,
+			login,
 			home,
 			profile,
 			create,
@@ -74,7 +86,7 @@ export class RouterUtilsService {
 
 	/** @description Route navigation by name */
 	/** @TODO Completar o CATCH... */
-	navigateTo(routerName: string): void {
-		this.router.navigate([routerName]).catch(response => console.log(response));
+	navigateTo(routerName: string, extras?: NavigationExtras): void {
+		this.router.navigate([routerName], extras).catch(response => console.log(response));
 	}
 }
