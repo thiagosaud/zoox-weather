84477bb35bf4c8b985b8e657615a1a7ee6a34faf
THIAGO DE BONIS <thiagobonis@outlook.com>
2020-07-14 23:19:03 -0300

    Updated App Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
 	selector: 'zx-root',
 	template: '<router-outlet></router-outlet>',
-	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
 	constructor() {}
