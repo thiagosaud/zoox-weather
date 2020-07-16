3a02ce985befa13fdf8bb820f209431a1c941aaf
THIAGO DE BONIS <thiagobonis@outlook.com>
2020-07-15 21:59:33 -0300

    Created Utils Interfaces

---
+++ src/app/shared/interfaces/utils.interface.ts
@@ -0,0 +1,12 @@
+export interface ISelectItem {
+	cityCode: string;
+	cityName: string;
+	cityLat: string;
+	cityLon: string;
+	isClicked: boolean;
+}
+
+export interface ISelectOptionItem {
+	value: string;
+	text: string;
+}
