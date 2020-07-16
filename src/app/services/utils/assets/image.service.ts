83e249f09f03276a8093dda9dc7152261404b9ec
THIAGO DE BONIS <thiagobonis@outlook.com>
2020-07-16 02:49:41 -0300

    Updated Image Service for Weather Conditions

--- src/app/services/utils/assets/image.service.ts
+++ src/app/services/utils/assets/image.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 export class ImageAssetsUtilsService {
 	constructor() {}
 
-	get(name: string) {
+	get(name: string): string {
 		switch (name.toLowerCase()) {
 			case 'logo':
 				return '/assets/images/utils/logo.svg';
@@ -14,6 +14,33 @@ export class ImageAssetsUtilsService {
 				return '/assets/images/utils/earth.png';
 			case 'earth-locations':
 				return '/assets/images/utils/earth_locations.png';
+			default:
+				throw new Error('This image does not exist in assets!');
+		}
+	}
+
+	getWeatherCondition(name: string): string {
+		switch (name.toLowerCase()) {
+			case '01d':
+			case '01n':
+			case '02d':
+			case '02n':
+			case '03d':
+			case '03n':
+			case '04d':
+			case '04n':
+			case '09d':
+			case '09n':
+			case '10d':
+			case '10n':
+			case '11d':
+			case '11n':
+			case '13d':
+			case '13n':
+			case '50d':
+			case '50n':
+			case 'wind':
+				return `/assets/images/weather/${name.toLowerCase()}.png`;
 			default:
 				throw new Error('This image does not exist in assets!');
 		}
