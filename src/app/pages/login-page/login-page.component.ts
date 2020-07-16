09a879ce8f5b256e90493ab91a3e086f784486a0
THIAGO DE BONIS <thiagobonis@outlook.com>
2020-07-16 18:14:45 -0300

    Updated Login Page

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+// SERVICES
+import { AuthStoreService } from '@services/store/authentication/auth.service';
 
 @Component({
 	selector: 'zx-login-page',
-	templateUrl: './login-page.component.html',
+	template: '<zx-card-login (submitForm)="login($event)"></zx-card-login>',
 	styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent implements OnInit {
-	constructor() {}
+	constructor(protected readonly authStore: AuthStoreService) {}
 
 	ngOnInit(): void {}
+
+	login(loginForm: FormGroup): void {
+		if (loginForm.valid) {
+			const { login, password } = loginForm.value;
+			this.authStore.add(login, password);
+		}
+	}
 }
