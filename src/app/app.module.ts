e4d71900ccf73efc5ff8494165bfa0aa873cfce7
THIAGO DE BONIS <thiagobonis@outlook.com>
2020-07-16 17:47:31 -0300

    Added  RegisterLocaleData

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeBr from '@angular/common/locales/pt';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 // ROOT CONFIG
@@ -14,10 +16,13 @@ import { PagesModule } from '@pages/pages.module';
 // INTERCEPTORS
 import { HttpErrorInterceptor } from '@interceptors/http/http-error.interceptor';
 
+registerLocaleData(localeBr, 'pt');
+
 @NgModule({
 	declarations: [AppComponent],
 	imports: [BrowserModule, AppRoutingModule, NgrxModule, ToastifyModule, PagesModule],
 	providers: [
+		{ provide: LOCALE_ID, useValue: 'pt' },
 		{
 			provide: HTTP_INTERCEPTORS,
 			useClass: HttpErrorInterceptor,
