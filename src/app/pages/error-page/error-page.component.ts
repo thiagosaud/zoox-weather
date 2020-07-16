26784694499e99d648d232480b3ab96a4a62312b
THIAGO DE BONIS <thiagobonis@outlook.com>
2020-07-16 20:06:55 -0300

    Updated Pages Components

--- src/app/pages/error-page/error-page.component.ts
+++ src/app/pages/error-page/error-page.component.ts
@@ -1,12 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+// SERVICE
+import { RouterUtilsService } from '@services/utils/router/router.service';
+
 @Component({
 	selector: 'zx-error-page',
-	templateUrl: './error-page.component.html',
+	template: `
+		<section>
+			<span>404</span>
+			<span>Esta página não existe neste Planeta!</span>
+
+			<zx-button (click)="backToRoute()" [ngClass]="['width-15', 'margin-top-2']">Voltar</zx-button>
+		</section>
+	`,
 	styleUrls: ['./error-page.component.scss'],
 })
 export class ErrorPageComponent implements OnInit {
-	constructor() {}
+	constructor(protected readonly routerService: RouterUtilsService) {}
 
 	ngOnInit(): void {}
+
+	backToRoute(): void {
+		this.routerService.navigateTo('/');
+	}
 }
--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 // SERVICES
+import { AuthUtilsService } from '@services/utils/authentication/auth.service';
 import { AuthStoreService } from '@services/store/authentication/auth.service';
 
 @Component({
@@ -10,9 +11,11 @@ import { AuthStoreService } from '@services/store/authentication/auth.service';
 	styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent implements OnInit {
-	constructor(protected readonly authStore: AuthStoreService) {}
+	constructor(protected readonly authService: AuthUtilsService, protected readonly authStore: AuthStoreService) {}
 
-	ngOnInit(): void {}
+	ngOnInit(): void {
+		this.authService.removeToken();
+	}
 
 	login(loginForm: FormGroup): void {
 		if (loginForm.valid) {
