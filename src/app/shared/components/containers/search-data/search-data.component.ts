cedf62f1d0836cb6a95bf8b491cc13ccd77c9f4e
THIAGO DE BONIS <thiagobonis@outlook.com>
2020-07-17 21:53:03 -0300

    Refactoring Search Data Component

--- src/app/shared/components/containers/search-data/search-data.component.ts
+++ src/app/shared/components/containers/search-data/search-data.component.ts
@@ -1,26 +1,37 @@
-import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, Input, EventEmitter, OnDestroy } from '@angular/core';
 import { FormGroup, FormControlName } from '@angular/forms';
+import { Subscription, BehaviorSubject } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
 	selector: 'zx-search-data',
 	templateUrl: './search-data.component.html',
 	styleUrls: ['./search-data.component.scss'],
 })
-export class SearchDataComponent implements OnInit {
+export class SearchDataComponent implements OnInit, OnDestroy {
 	@Output() orderBy = new EventEmitter<boolean>();
 	@Input() fmGroup: FormGroup;
 	@Input() fmControlName: FormControlName;
 	@Input() minLength: number;
 	@Input() maxLength: number;
 	@Input() placeholder: string;
-	isOrderByAsc = true;
+	protected subscription$: Subscription;
+	isOrderByAsc$ = new BehaviorSubject<boolean>(true);
 
 	constructor() {}
 
-	ngOnInit(): void {}
+	ngOnInit(): void {
+		// If the user has activated the ordering and type in the form, the ordering status returns to the default.
+		this.subscription$ = this.fmGroup.valueChanges.pipe(filter(({ search }) => !!search)).subscribe(() => this.isOrderByAsc$.next(true));
+	}
+
+	ngOnDestroy(): void {
+		this.subscription$.unsubscribe();
+		this.isOrderByAsc$.unsubscribe();
+	}
 
 	onclickOrderBy(): void {
-		this.isOrderByAsc = !this.isOrderByAsc;
-		this.orderBy.emit(this.isOrderByAsc);
+		this.isOrderByAsc$.next(!this.isOrderByAsc$.getValue());
+		this.orderBy.emit(this.isOrderByAsc$.getValue());
 	}
 }
